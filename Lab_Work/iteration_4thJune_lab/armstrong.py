f116fa2e355d08d85a70135f91adfaf0a64f0503
HarshitShukla26 <harshitmzpup65@gmail.com>
2026-06-04 14:24:24 +0530

    armstrong

---
+++ Lab_Work/iteration_4thJune_lab/armstrong.py
@@ -0,0 +1,24 @@
+# Program to check if a number is an Armstrong number
+num = int(input("Enter a number: "))
+
+temp = num
+count = 0
+
+# Count digits
+while temp > 0:
+    count = count + 1
+    temp = temp // 10
+
+temp = num
+sum = 0
+
+# Calculate sum of digits raised to count
+while temp > 0:
+    digit = temp % 10
+    sum = sum + digit ** count
+    temp = temp // 10
+
+if sum == num:
+    print(num, "is an Armstrong Number")
+else:
+    print(num, "is Not an Armstrong Number")
